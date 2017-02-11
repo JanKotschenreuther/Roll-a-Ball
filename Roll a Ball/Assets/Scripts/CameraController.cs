912b52bd0fa459ca49e3debc7138206c2398286e
JanKotschenreuther <JanKotschenreuther@users.noreply.github.com>
2017-02-11 19:49:50 +0100

    finished Tutorial

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 offset;
+    public GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private int count;
+
+    public float Speed = 1;
+    public Text countText;
+    public Text winText;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        rigidBody.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * Speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+
+            if (count >= 8)
+            {
+                winText.text = "You Win!";
+            }
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count;
+    }
+}
